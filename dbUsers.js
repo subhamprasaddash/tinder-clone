91c660af9c5187020ce077dcc5ad7e6e3a0b2ed9
subham.dash <subham.dash@xarvis.ai>
2021-07-25 12:47:08 +0530

    version 0.1

--- dbUsers.js
+++ dbUsers.js
@@ -1,11 +1,47 @@
-import { Double } from 'bson';
+
 import mongoose from 'mongoose';
 
-const userSchema = mongoose.Schema({
-    name: String,
-    imageUrl: String,
-    age: Double,
-    about: String
-})
 
-export default mongoose.model("users", userSchema);
+// Create Schemat
+const userSchema = mongoose.Schema({
+    name: { 
+        type: String,
+        required: true
+    },
+    imageUrl:  { 
+        type: String,
+        required: true
+    },
+    age:  { 
+        type: String,
+        required: true
+    } ,
+    about: { 
+        type: String,
+        default: 'Nothing to display'
+    },
+})
+//Collection created
+const db = new mongoose.model("users", userSchema);
+
+//Add data to collection
+/* const createCollection = async() =>{
+
+    try{
+        const user1 = new db ({
+            name:'Mahendra Singh Dhoni',
+            age: '35',
+            imageUrl:'https://cdn.dnaindia.com/sites/default/files/styles/full/public/2021/06/01/976753-715005-ms-dhoni.jpg',
+            about: 'Former Captain'
+        })
+
+        const result = await user1.save();
+        console.log(result)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+createCollection();
+ */
+export default db;  
--- server.js
+++ server.js
@@ -2,26 +2,67 @@ import express from 'express';
 import mongoose from 'mongoose';
 import path from 'path';
 import cors from 'cors';
+import db from './dbUsers.js';
 
 //App config 
 
 const app = express();
 var __dirname = path.resolve();
 
-// Middleware
+//Middleware
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
 
-//database config
 
-mongoose.connect('mongodb+srv://admin:CQtq7r5fEIuStn6x@cluster0.agf44.mongodb.net/tinder-clone-db?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true});
+//database config 
+
+mongoose.connect('mongodb+srv://admin:CQtq7r5fEIuStn6x@cluster0.agf44.mongodb.net/tinder-clone-db?retryWrites=true&w=majority',{useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true})
+        .then(()=> console.log('Database connected'))
+        .catch((err) => console.log(err))
 
 //API endpoints
+
 app.get('/', (req, res) => {
-    res.status(200).json('Connection successful');
+    res.status(200).json('API working');
 })
+
+app.post('/users', (req, res) => {
+  const user = req.body;
+
+  db.create( user , (err, data) =>{
+    if(err){
+      res.status(500).json(err)
+    }else{
+      res.status(201).json(data)
+      console.log(data)
+    }
+
+  })
+});
+
+app.get('/users', (req,res) => {
+    db.find((err, data) =>{
+    if(err){
+      res.status(500).json(err)
+    }else{
+      res.status(200).json(data)
+    }
+
+  })
+});
+
+/* app.delete('/users', (req,res) => {
+  users.remove((err, data) => {
+      if(err){
+      res.status(500).json(err)
+    }else{
+      res.status(200).json(data)
+    }
+  })
+}) */
+
 //app.listen
 
 let port = process.env.PORT;
